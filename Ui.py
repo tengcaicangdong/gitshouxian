d8bbda6dd3d6579f2eb0e04132a5e7311e247f39
tengcai <3073754544@qq.com>
2024-11-01 17:59:09 +0800

    从零开始--1

---
+++ Ui.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QApplication,QMainWindow
+
+class MainWin(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+
+
---
+++ git.py
@@ -0,0 +1,12 @@
+"""
+1.管理文件   在 gitbash “git  init”
+
+.git文件 存储各种数据
+
+
+2.“git status”  检测文件状态
+
+3.“git add”   添加文件   “git add .”所有
+
+4.“git commit -m '描述信息'  ”
+"""
