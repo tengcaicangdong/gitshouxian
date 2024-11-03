6a6e6a685e6eae2c2d4a7b7e7bab471e77c7925d
tengcai <3073754544@qq.com>
2024-11-03 17:21:08 +0800

    学习v3

--- git.py
+++ git.py
@@ -1,4 +1,5 @@
 """
+a
 1.管理文件   在 gitbash “git  init”
 
 .git文件 存储各种数据
@@ -17,4 +18,22 @@
 
 
 6.git reflog  可以看到所有的版本
+
+b
+
+1.查看分支 git branch  2.创建分支 git branch 名字
+
+3.git marge 要合并的分支  4.删除分支 git branch -d 名字 5.切换分支 git checkout mz
+
+c
+
+1.给远程仓库起别名 git remote add origin 地址
+
+2.向远端推送代码 git push -u origin  分支
+
+3.拉取代码 git pull origin dev
+
+4.克隆代码 git clone 地址
+
+5.提交代码 git push origin 分支名字
 """
